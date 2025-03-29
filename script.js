855f60dc291079f2d782452394edaae530e8692a
VINNSTOREE <vingg1633@gmail.com>
2025-03-29 16:25:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function copyText(button) {
+    let inputField = button.previousElementSibling;
+    inputField.select();
+    inputField.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(inputField.value);
+
+    button.textContent = "✔️ Disalin!";
+    setTimeout(() => {
+        button.textContent = "📋 Salin Paket";
+    }, 2000);
+}
